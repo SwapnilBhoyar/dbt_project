a6dd49fb549f607e061a6b11501944210fa5f110
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2022-05-29 21:32:10 +0700

    commit

--- learn-dbt/models/example/my_first_dbt_model.sql
+++ learn-dbt/models/example/my_first_dbt_model.sql
@@ -3,12 +3,7 @@
 
 with source_data as (
 
-    select 1 as id, 'NJ' as state, '2020-02-01 00:01:00.000'::timestamp as updated_at
-    union all
-    select null as id, 'CT' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
-    union all
-    select 4 as id, 'VT' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
-
+    select * from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER;
 )
 
 select *
