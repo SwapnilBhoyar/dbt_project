a61b8289747e85f888c4fc8c969b5aaae63052e7
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2022-05-30 06:38:21 +0700

    commit

--- learn-dbt/models/example/my_first_dbt_model.sql
+++ learn-dbt/models/example/my_first_dbt_model.sql
@@ -1,6 +1,3 @@
-res resultset;
-col_name varchar;
-select_statement varchar;
 begin
     col_name := 'C_NAME';
     select_statement:= 'SELECT' || col_name || 'FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER';
