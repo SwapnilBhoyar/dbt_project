f63322827e8bbcb3564c18f86d10a2e0409902a7
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2022-05-29 22:56:58 +0700

    commit

--- learn-dbt/models/example/my_first_dbt_model.sql
+++ learn-dbt/models/example/my_first_dbt_model.sql
@@ -1,17 +1,17 @@
 
 {{ config(materialized='table', alias='first_model', tags=["nightly", "example"] ) }}
 
-execute immediate $$
-    declare
-        res resultset;
-        col_name varchar;
-        select_statement varchar;
-    begin
-        col_name := 'C_NAME';
-        select_statement:= 'SELECT' || col_name || 'FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER';
-        res := (execute immediate : select_statement);
-        return table(res);
-    end;
-$$;
+
+declare
+    res resultset;
+    col_name varchar;
+    select_statement varchar;
+begin
+    col_name := 'C_NAME';
+    select_statement:= 'SELECT' || col_name || 'FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER';
+    res := (execute immediate : select_statement);
+    return table(res);
+end;
+
 
  
