1ead10d5dcf84026e90983828a09a20400b963c8
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2022-05-30 11:32:06 +0700

    "commit"

--- learn-dbt/models/example/my_first_dbt_model.sql
+++ learn-dbt/models/example/my_first_dbt_model.sql
@@ -1,7 +1,6 @@
 {{ config(materialized='table', alias='first_model', tags=["nightly", "example"] ) }}
 
-CREATE or replace procedure output_message()
-language sql
+CREATE procedure output_message()
 as
 begin
   select * from FROM "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."CUSTOMER"
