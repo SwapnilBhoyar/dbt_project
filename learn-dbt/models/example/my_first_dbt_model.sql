b1e9ce73809d2d7bbdc09f696cb6897fe7783190
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2022-05-30 09:49:31 +0700

    "commit"

--- learn-dbt/models/example/my_first_dbt_model.sql
+++ learn-dbt/models/example/my_first_dbt_model.sql
@@ -1,9 +1,3 @@
-begin
-    col_name := 'C_NAME';
-    select_statement:= 'SELECT' || col_name || 'FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER';
-    res := (execute immediate : select_statement);
-    return table(res);
-end;
+{{ config(materialized='table', alias='first_model', tags=["nightly", "example"] ) }}
 
-
-  
+{{get_data()}}
